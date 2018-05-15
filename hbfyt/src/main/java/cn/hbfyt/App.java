d1b8190f61cfa29ff35b45a100139a917439daa9
huangbo <huangbo@huangbodeMacBook-Pro.local>
2018-05-15 17:14:06 +0800

    add

--- hbfyt/src/main/java/cn/hbfyt/App.java
+++ hbfyt/src/main/java/cn/hbfyt/App.java
@@ -2,6 +2,9 @@ package cn.hbfyt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +15,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class App {
+public class App implements EmbeddedServletContainerCustomizer {
     public static void main(String[] args) {
         // System.out.println("Hello World!");
         SpringApplication.run(App.class, args);
+    }
+
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(80);
     }
 }
