d540830ed9e816445095b0489b4d8af4535ec0ab
huangbo <huangbo@huangbodeMacBook-Pro.local>
2018-05-15 17:02:24 +0800

    add

--- hbfyt/src/main/java/cn/hbfyt/HelloWorldController.java
+++ hbfyt/src/main/java/cn/hbfyt/HelloWorldController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+// @RequestMapping("/hello")
 public class HelloWorldController {
-    @RequestMapping("/world")
+    @RequestMapping("/")
     public String helloWorld() {
-        return "helloWorld";
+        return "你好啊，婷儿！快下班了，哈哈哈哈哈哈哈哈哈";
     }
 }
