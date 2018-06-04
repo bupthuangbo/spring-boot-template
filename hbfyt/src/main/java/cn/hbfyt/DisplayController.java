8270de0fd21179e29287156ee7dc204d8e173506
huangbo <huangbo@huangbodeMacBook-Pro.local>
2018-06-04 15:35:32 +0800

    debug

--- hbfyt/src/main/java/cn/hbfyt/DisplayController.java
+++ hbfyt/src/main/java/cn/hbfyt/DisplayController.java
@@ -3,6 +3,8 @@ package cn.hbfyt;
 import java.io.File;
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DisplayController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DisplayController.class);
 
     @RequestMapping(value = "/pic", method = RequestMethod.GET)
     public ModelAndView index() {
@@ -19,6 +22,7 @@ public class DisplayController {
         fileList = getFiles();
         view.addObject("src", fileList);
         view.addObject("num", fileList.size());
+        LOGGER.info("pic num is " + fileList.size());
         return view;
     }
 
